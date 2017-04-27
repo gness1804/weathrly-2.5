5b67b52836edadaf0c158fa284036e30f84df90f
gness1804 <graham.nessler@gmail.com>
2017-04-27 15:43:54 -0500

    Add header and main components

--- App.js
+++ App.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import {
-  View,
-  Text,
-} from 'react-native';
+import { View } from 'react-native';
 import styles from './styles/App-styles';
 import Header from './components/Header';
-// import Main from './components/Main';
+import Main from './components/Main';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Header />
+        <Main />
       </View>
     );
   }
--- components/Header.js
+++ components/Header.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native'
 
 class Header extends Component {
-    render() {
-        return (
-            <View>
-                <Text>I am the header component.</Text>
-            </View>
-        );
-    }
+  render() {
+    return (
+      <View>
+        <Text>I am the Header component.</Text>
+      </View>
+    );
+  }
 }
 
-export default Header;
+export default Header
