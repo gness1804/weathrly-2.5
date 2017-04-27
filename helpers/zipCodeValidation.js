5bdb3f8781d40eaecb03f06693abf1c8c4f79196
gness1804 <graham.nessler@gmail.com>
2017-04-27 16:12:26 -0500

    Add typechecking to zip code validation function

--- helpers/zipCodeValidation.js
+++ helpers/zipCodeValidation.js
@@ -1,4 +1,6 @@
-const zipCodeIsValid = (zip) => {
+// @flow
+
+const zipCodeIsValid = (zip: number): boolean => {
   const parsedZip = parseInt(zip, 10)
   if (!parsedZip || typeof parsedZip !== 'number' || zip.length !== 5) {
     return false
