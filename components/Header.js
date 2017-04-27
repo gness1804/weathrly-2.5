234449e6c9b43ab97991848f2bc99565da6b2062
gness1804 <graham.nessler@gmail.com>
2017-04-27 15:47:32 -0500

    Add data to header from old project

--- components/Header.js
+++ components/Header.js
@@ -1,14 +1,29 @@
-import React, { Component } from 'react';
-import { View, Text } from 'react-native'
+import React from 'react';
+import {
+  Text,
+  View,
+  Image,
+  Linking,
+  TouchableOpacity,
+} from 'react-native';
+import styles from '../styles/header-styles';
 
-class Header extends Component {
-  render() {
-    return (
-      <View>
-        <Text>I am the Header component.</Text>
-      </View>
-    );
-  }
+const Header = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.headline}>Welcome to Weathrly</Text>
+      <Text style={styles.tagline}>Your World, Your Weather.</Text>
+      <TouchableOpacity
+        onPress={() => { Linking.openURL('https://www.wunderground.com/') }}
+      >
+        <Image
+          style={styles.pic}
+          source={{ uri: 'https://icons.wxug.com/logos/PNG/wundergroundLogo_4c.png' }}
+        />
+      </TouchableOpacity>
+      <Text style={styles.credit}>Data courtesy of Weather Underground.</Text>
+    </View>
+  );
 }
 
-export default Header
+export default Header;
---
+++ styles/header-styles.js
@@ -0,0 +1,27 @@
+const styles = {
+  container: {
+    alignItems: 'center',
+  },
+  credit: {
+    marginTop: 10,
+  },
+  headline: {
+    fontSize: 30,
+    fontWeight: '700',
+    marginBottom: 10,
+    marginTop: 20,
+    textAlign: 'center',
+  },
+  pic: {
+    height: 70,
+    marginTop: 10,
+    width: 100,
+  },
+  tagline: {
+    fontSize: 20,
+    fontStyle: 'italic',
+    textAlign: 'center',
+  },
+}
+
+export default styles
