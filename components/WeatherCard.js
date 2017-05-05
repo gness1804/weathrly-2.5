076a1969598c8a70c8bf391748ab7b8c5b51e153
gness1804 <graham.nessler@gmail.com>
2017-05-05 18:26:32 -0500

    Destructure the props in WeatherCard

--- components/WeatherCard.js
+++ components/WeatherCard.js
@@ -7,16 +7,16 @@ import {
 import styles from '../styles/weather-card-styles';
 
 const WeatherCard = ({ ...props }) => {
-  const pic = props.icon_url
+  const { icon_url, title, fcttext } = props
   return (
     <View style={styles.container}>
       <Image
         style={styles.pic}
-        source={{ uri: `${pic}` }}
+        source={{ uri: `${icon_url}` }} //eslint-disable-line
       />
-      <Text style={styles.timePeriod}>Time Period: {props.title}</Text>
+      <Text style={styles.timePeriod}>Time Period: {title}</Text>
       <Text style={styles.forecast}>
-        <Text style={styles.textSpan}>Forecast:</Text> {props.fcttext}</Text>
+        <Text style={styles.textSpan}>Forecast:</Text> {fcttext}</Text>
     </View>
   );
 }
