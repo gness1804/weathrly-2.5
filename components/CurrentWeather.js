ca6991550a65984e1876ed2d7183e6ee3eaa7e91
gness1804 <graham.nessler@gmail.com>
2017-05-05 20:39:17 -0500

    Add color coding of temp based on city

--- components/CurrentWeather.js
+++ components/CurrentWeather.js
@@ -10,12 +10,23 @@ import styles from '../styles/current-weather-styles';
 
 const CurrentWeather = ({ ...props }: Object) => {
   const { currentTemp, location } = props
+  let degreeStyle
+  if (currentTemp < 50) {
+    degreeStyle = styles.cold
+  } else if (currentTemp < 65) {
+    degreeStyle = styles.chilly
+  } else if (currentTemp < 80) {
+    degreeStyle = styles.ideal
+  } else {
+    degreeStyle = styles.hot
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.headline}>
           Current temperature in {capitalize(location)}:
       </Text>
-      <Text style={styles.degreeReadout}>{Math.round(currentTemp).toString()} &deg; F</Text>
+      <Text style={degreeStyle}>{Math.round(currentTemp).toString()} &deg; F</Text>
     </View>
   )
 }
--- styles/current-weather-styles.js
+++ styles/current-weather-styles.js
@@ -1,17 +1,34 @@
 import { StyleSheet } from 'react-native';
 
+const degreeReadout = {
+  fontSize: 32,
+}
+
 const styles = StyleSheet.create({
+  chilly: {
+    color: '#2E7CC7',
+    fontSize: degreeReadout.fontSize,
+  },
+  cold: {
+    color: '#180FC1',
+    fontSize: degreeReadout.fontSize,
+  },
   container: {
     alignItems: 'center',
     marginTop: 30,
-  },
-  degreeReadout: {
-    fontSize: 26,
   },
   headline: {
     fontSize: 20,
     fontWeight: '600',
     marginBottom: 20,
+  },
+  hot: {
+    color: '#EB2C08',
+    fontSize: degreeReadout.fontSize,
+  },
+  ideal: {
+    color: '#238623',
+    fontSize: degreeReadout.fontSize,
   },
 })
 
