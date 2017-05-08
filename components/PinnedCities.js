4f9813df201299288bc2e1216be0ea04174b069b
gness1804 <graham.nessler@gmail.com>
2017-05-08 12:00:09 -0500

    Add cities state to PinnedCities

--- components/PinnedCities.js
+++ components/PinnedCities.js
@@ -1,7 +1,20 @@
+// @flow
+
 import React, { Component } from 'react';
 import { View, Text } from 'react-native'
 
 class PinnedCities extends Component {
+  constructor(props: Object) {
+    super(props);
+    this.state = {
+      cities: [],
+    }
+  }
+
+  state: {
+    cities: Array<Object>,
+  }
+
   render() {
     return (
       <View>
